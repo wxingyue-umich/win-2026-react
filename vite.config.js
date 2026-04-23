e784d408f473f7dff616dee832b601c1153c7f7b
wxingyue-umich <wxingyue@umich.edu>
2026-04-23 19:56:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/w26-react-deliverable/',
+  base:'/win-2026-react/',
 })
